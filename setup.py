190f86dfd8cfe221cb95bf10081f700156345c75
William Trevor Olson <wtolson@gmail.com>
2012-01-14 15:44:04 -0700

    Setup in python package form.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Pysis',
+    version='0.1.0',
+    author='William Trevor Olson',
+    author_email='wtolson@gmail.com',
+    packages=['pysis'],
+    scripts=[],
+    url='',
+    license='LICENSE.txt',
+    description='Useful toolkit for working with Isis in Python.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "PyYAML>=3.10"
+    ],
+)
