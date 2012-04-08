71b08fdeeec356680a496f573198215315ab0dc4
William Trevor Olson <wtolson@gmail.com>
2012-04-08 09:02:50 -0700

    Added the functionality of the isis startup script to the module root.

--- pysis/__init__.py
+++ pysis/__init__.py
@@ -1,19 +1,46 @@
 # -*- coding: utf-8 -*-
 import os, sys
+from os import path
 
-ISIS_ROOT = os.environ.get('ISISROOT')
-if ISIS_ROOT is None:
-    sys.stderr.write('Warning! ISISROOT is not defined. Bitch.\n')
+ISIS_ROOT = os.environ.setdefault('ISISROOT', '/usgs/pkgs/isis3/isis')
+try:
+    with open(filename) as _f:
+        ISIS_VERSION = _f.readline().strip()
+
+except:
+    sys.stderr.write('Warning! Could not find isis. Is `ISISROOT` set?\n')
 
     (ISIS_VERSION, ISIS_VERSION_MAJOR, ISIS_VERSION_MINOR,
         ISIS_VERSION_PATCH, ISIS_VERSION_BUILD) = 5 * (None,)
 
 else:
-    with open(filename) as _f:
-        ISIS_VERSION = _f.readline().strip()
-
     (ISIS_VERSION_MAJOR, ISIS_VERSION_MINOR,
         ISIS_VERSION_PATCH, ISIS_VERSION_BUILD) = map(int, version.split('.'))
+
+
+if ISIS_VERSION_MAJOR == 3:
+    #Check for the ISIS3DATA directory. If it does not exist use a default
+    ISIS_DATA = path.normpath(path.join(ISIS_ROOT, '../data'))
+    if not path.exists(ISIS_DATA):
+        ISIS_DATA = '/usgs/cpkgs/isis3/data'
+    os.environ['ISIS3DATA'] = ISIS_DATA
+
+    #Check for the ISIS3TESTDATA directory. If it does not exist use a default
+    ISIS_TEST_DATA = path.normpath(path.join(ISIS_ROOT, '../testData'))
+    if not path.exists(ISIS_TEST_DATA):
+        ISIS_DATA = '/usgs/cpkgs/isis3/testData'
+    os.environ['ISIS3DATA'] = ISIS_DATA
+
+    #If PATH is not set, just set it to a default location. Else preappend
+    #the isis path to the end of the current path
+    ISIS_PATH = path.join(ISIS_ROOT, 'bin')
+    if environ.get('PATH'):
+        os.environ['PATH'] = '%s:%s' % (ISIS_ROOT, os.environ['PATH'])
+    else:
+        os.environ['PATH'] = ISIS_PATH
+
+    QT_PLUGIN_PATH = path.join(ISIS_ROOT, '3rdParty/plugins')
+    os.environ['QT_PLUGIN_PATH'] = QT_PLUGIN_PATH
 
 
 def require_isis_version(major, minor=None, patch=None, build=None):
