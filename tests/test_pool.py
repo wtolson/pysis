6ae24fdcd56c2c3cbff93d6c58489437c64035d0
William Trevor Olson <wtolson@gmail.com>
2015-03-22 16:25:06 -0700

    Test isis pool.

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
-import pytest
+from pysis import IsisPool
 
 
 def test_pool():
-    pytest.xfail('unimplemented')
+    with IsisPool() as isis_pool:
+        res1 = isis_pool.isis_echo(count=1)
+        res2 = isis_pool.isis_echo(count=2)
+        res3 = isis_pool.isis_echo(count=3)
+
+    assert res1.get() == 'count=1\n'
+    assert res2.get() == 'count=2\n'
+    assert res3.get() == 'count=3\n'
